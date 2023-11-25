10f02decf9a6e7d3511e9b5eaf82d70aa4ea8f49
Home <reikrb@gmail.com>
2023-11-25 18:27:40 +0100

    Adds render Method

---
+++ src/gameRender.js
@@ -0,0 +1,21 @@
+import globals from "./globals.js"
+import {Game} from "./constants.js"
+
+//Graphic Renderer Method
+export default function render(){
+
+    //Modifies Game Depending On Game State
+    switch(globals.gameState){
+        case Game.LOADING:
+            //Draws Loading
+            break;
+        
+        case Game.PLAYING:
+            drawGame();
+            break;
+        
+        default:
+            console.error("Error: Game State invalid")
+
+    }
+}
