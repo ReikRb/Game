b392e1fb6162e029ac6881ee43df06761cb44b37
Home <reikrb@gmail.com>
2023-11-25 18:19:04 +0100

    Adds update Method

---
+++ src/gameLogic.js
@@ -0,0 +1,21 @@
+import globals from "./globals.js"
+import {Game} from "./constants.js"
+
+export default function update(){
+
+    //Modifies Game Depending On Game State
+    switch(globals.gameState){
+        case Game.LOADING:
+            console.log("Loading assets...");
+            break;
+        
+        case Game.PLAYING:
+            playGame();
+            break;
+        
+        default:
+            console.error("Error: Game State invalid")
+            
+    }
+
+}
