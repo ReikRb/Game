ce917746c4261f016f689559ac0e7e251cfad40e
Home <reikrb@gmail.com>
2023-11-26 18:00:13 +0100

    Adds Frames Class

---
+++ src/Frames.js
@@ -0,0 +1,7 @@
+export default class Frames {
+    constructor(framesPerState){
+
+        this.framesPerState = framesPerState; //Frames Quantity of Animation State
+        this.frameCounter   = 0;              // Frames counter
+    }
+}
