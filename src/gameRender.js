64f1162124b42b8ed2970918f9a46d1f3e65e1d7
Home <reikrb@gmail.com>
2023-11-26 19:20:54 +0100

    Adds renderSprite & drawSprites Method & adds the last to drawGame.

--- src/gameRender.js
+++ src/gameRender.js
@@ -26,6 +26,37 @@ function drawGame(){
     globals.ctx.clearRect(0,0, globals.canvas.width, globals.canvas.height)
 
 
-    //Draws FPS in Screen
-    globals.ctx.fillText("FPS: " + (1 / globals.deltaTime), 30, 30)
+    // //Draws FPS in Screen
+    // globals.ctx.fillText("FPS: " + (1 / globals.deltaTime), 30, 30)
+
+    //Draw Elements
+    drawSprites();
+}
+
+function drawSprites() {
+    for (let i = 0; i < globals.sprites.length; i++) {
+        const sprite = globals.sprites[i];
+
+        renderSprite(sprite)
+    }
+}
+
+
+function renderSprite(sprite){
+    //Calculates tilemap drawing position
+    const xTile = sprite.frames.frameCounter * sprite.imageSet.gridSize + sprite.imageSet.xOffset
+    const yTile = sprite.state * sprite.imageSet.gridSize + sprite.imageSet.yOffset
+
+    // Sprites position rounded down
+    const xPos = Math.floor(sprite.xPos)
+    const yPos = Math.floor(sprite.yPos)
+
+    //Draws new frame on proper position
+    globals.ctx.drawImage(
+        globals.tileSet,                                //Img File
+        xTile, yTile,                                   //X & Y Position Source
+        sprite.imageSet.xSize, sprite.imageSet.ySize,   //Height & Width Source
+        xPos, yPos,                                     //Final X & Y Position
+        sprite.imageSet.xSize, sprite.imageSet.ySize    //Final Height & Width
+    )
 }
