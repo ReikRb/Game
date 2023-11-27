971b4fc2582124c160eac8714055432fdb58f377
ReikRb <reikrb@gmail.com>
2023-11-27 18:20:42 +0100

    Adds Level class & level1 constant and exports them

--- src/Level.js
+++ src/Level.js
@@ -1,5 +1,17 @@
 class Level {
     constructor(data, imageSet){
         this.data     = data
+        this.imageSet = imageSet
     }
 }
+
+const level1 = [
+    [12,12,12,12,12,12,12,12],
+    [12,12,12,12,12,12,12,12],
+    [12,12,12,12,12,12,12,12],
+    [12,12,12,12,12,12,12,12],
+    [ 2, 2, 2, 2, 2, 2, 2, 2],
+    [10,10,10,10,10,10,10,10]
+]
+
+export {Level, level1}
